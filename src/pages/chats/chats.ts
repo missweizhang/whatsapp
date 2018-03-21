e2eab76ac6f46b8e5658a7d395b732b53b9d05db
Wei Zhang <missweizhang@gmail.com>
2018-03-21 01:41:21 -0700

    add chat model and message model (step 2)

--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs';
 import * as moment from 'moment';
+import { Chat, MessageType } from '../../models';
 
 /**
  * Generated class for the ChatsPage page.
@@ -15,7 +16,7 @@ import * as moment from 'moment';
   templateUrl: 'chats.html',
 })
 export class ChatsPage {
-  chats: Observable<any[]>; 
+  chats: Observable<Chat[]>; 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.chats = this.findChats();
@@ -25,7 +26,7 @@ export class ChatsPage {
     console.log('ionViewDidLoad ChatsPage');
   }
 
-  private findChats(): Observable<any[]> {
+  private findChats(): Observable<Chat[]> {
     return Observable.of([
       {
         _id: '0',
@@ -33,7 +34,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
         lastMessage: {
           content: 'You on your way?',
-          createdAt: moment().subtract(1, 'hours').toDate()
+          createdAt: moment().subtract(1, 'hours').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -42,7 +44,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
         lastMessage: {
           content: 'Hey, it\'s me',
-          createdAt: moment().subtract(2, 'hours').toDate()
+          createdAt: moment().subtract(2, 'hours').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -51,7 +54,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
         lastMessage: {
           content: 'I should buy a boat',
-          createdAt: moment().subtract(1, 'days').toDate()
+          createdAt: moment().subtract(1, 'days').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -60,7 +64,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
         lastMessage: {
           content: 'Look at my mukluks!',
-          createdAt: moment().subtract(4, 'days').toDate()
+          createdAt: moment().subtract(4, 'days').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -69,7 +74,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
         lastMessage: {
           content: 'This is wicked good ice cream.',
-          createdAt: moment().subtract(2, 'weeks').toDate()
+          createdAt: moment().subtract(2, 'weeks').toDate(),
+          type: MessageType.TEXT
         }
       }
     ]);
